6917745e749849c3ed37b870273de0e8af9dae8c
claytonfollett2021 <813551@mystma.org>
2021-02-19 16:04:44 +0000

    list10 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def list10(a):  
+    
+    if a[len(a) - 1] == 10: 
+       return True
+   
+    else: 
+        return False
+
+print (list10([1, 2, 3, 4, 5, 10,]))
